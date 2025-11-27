9b5df17e02ffbf23a3073a969228582b879dd3eb
rauphel <rauphelcyreuskenept@gmail.com>
2025-11-27 09:29:21 -0600

    debugging terrain

--- scripts/terrain.js
+++ scripts/terrain.js
@@ -43,8 +43,11 @@ function showTerrain(originVector) {
   for (let z = 0; z < rows - 1; z++) { // generates a triangles strip to display terrain using generated heights in 2d array
     beginShape(TRIANGLE_STRIP);
     for (let x = 0; x < cols; x++) {
-      vertex((x + originVector.x) * SUBDIVISIONS, (z + originVector.z) * SUBDIVISIONS, terrainHeight[50 + z + originVector.z][50 + x + originVector.x]);
-      vertex((x + originVector.x) * SUBDIVISIONS, (z + 1 + originVector.z) * SUBDIVISIONS, terrainHeight[50 + z + 1 + originVector.z][50 + x + originVector.x]);
+      if (Math.abs(originVector.x) < 40 && Math.abs(originVector.z < 40)) {
+        vertex((x + originVector.x) * SUBDIVISIONS, (z + originVector.z) * SUBDIVISIONS, terrainHeight[50 + z + originVector.z][50 + x + originVector.x]);
+        vertex((x + originVector.x) * SUBDIVISIONS, (z + 1 + originVector.z) * SUBDIVISIONS, terrainHeight[50 + z + 1 + originVector.z][50 + x + originVector.x]);
+
+      }
     }
     endShape();
   }
